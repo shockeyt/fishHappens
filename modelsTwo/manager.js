74378faa0545a5a355938acdb03acba57b030a17
shockeyt <shockeyt@gmail.com>
2017-05-11 20:18:18 -0600

    removed modelstwo folder

--- modelsTwo/manager.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, Sequelize){
-  var model = sequelize.define("manager", {
-    name: Sequelize.STRING,
-    email: Sequelize.STRING,
-    office_number: Sequelize.STRING,
-    cell_phone_number: Sequelize.STRING
-  });
-  return model;
-};
--- modelsTwo/song.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, Sequelize){
-  var model = sequelize.define("song", {
-    title: Sequelize.STRING,
-    duration: Sequelize.STRING,
-    date_of_release: Sequelize.STRING,
-    album_title: Sequelize.STRING
-  });
-  return model;
-};
